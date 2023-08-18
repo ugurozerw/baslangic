94284253e2d3fd20a3d5a055aff58dcaa97a2f96
Ugur <ugurozerw@gmail.com>
2023-08-18 16:24:53 +0300

    u

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,30 +1,34 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import "./cssforpages.css"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function BasicExample() {
+function RandomImageGenerator() {
+  const [randomNum, setRandomNum] = useState(null);
+
+  useEffect(() => {
+    generateRandomNumber();
+  }, []);
+
+  const generateRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    setRandomNum(randomNumber);
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div>
+      {randomNum !== null && (
+        <div>
+            <img
+              src={`https://picsum.photos/id/${randomNum}/750/900`}
+              alt={`Rastgele Resim ${randomNum}`}
+            />
+            <button>
+              <Link to="/">Anasayfa</Link>
+            </button>
+        </div>
+      )}
+          </div>
+      
   );
 }
 
-export default BasicExample;
+export default RandomImageGenerator;
