87dd83bb00e47b2bb315b7fb1f2d3b7be2677140
Ugur <ugurozerw@gmail.com>
2023-08-23 14:33:34 +0300

    s

--- src/components/RightColumn1Items.jsx
+++ src/components/RightColumn1Items.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 const RightColumn1Items = () => {
   return (
     <div className="button-container ">
-      <div className="custom-button-wrapper">
-        <button className="custom-button">Yeni Çikanlar</button>
+      <div>
+        <button className="sort-btns">Yeni Çikanlar</button>
       </div>
-      <div className="custom-button-wrapper">
-        <button className="custom-button">İndirimler</button>
+      <div>
+        <button className="sort-btns">İndirimler</button>
       </div>
-      <div className="custom-button-wrapper">
-        <button className="custom-button">En ucuzdan sırala</button>
+      <div>
+        <button className="sort-btns">En ucuzdan sırala</button>
       </div>
-      <div className="custom-button-wrapper">
-        <button className="custom-button">En pahalıdan sırala</button>
+      <div>
+        <button className="sort-btns">En pahalıdan sırala</button>
       </div>
-      <div className="custom-button-wrapper">
-        <button className="custom-button">Lorem ipsum dolor sit amet.</button>
+      <div>
+        <button className="sort-btns">Lorem ipsum dolor sit amet.</button>
       </div>
-      <div className="custom-button-wrapper">
-        <button className="custom-button">Lorem ipsum</button>
+      <div>
+        <button className="sort-btns">Lorem ipsum</button>
       </div>
     </div>
   );
