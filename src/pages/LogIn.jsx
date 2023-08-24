a7a823f6fa539fcd8acd913211069619bcb01048
Ugur <ugurozerw@gmail.com>
2023-08-24 11:13:19 +0300

    val-update

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -13,9 +13,7 @@ const brokenImageNumbers = [138, 286, 968, 394, 561, 751, 332, 589, 207, 587,
 
 function RandomImageGenerator() {
   const [randomNum, setRandomNum] = useState(null);
-  const [isInvalid, setIsInvalid] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+
 
   useEffect(() => {
     generateRandomNumber();
@@ -30,82 +28,111 @@ function RandomImageGenerator() {
     setRandomNum(randomNumber);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+const [isInvalid, setIsInvalid] = useState(false);
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
 
-    // Örnek email ve şifre doğrulaması
-    if (email === 'example@example.com' && password === 'password123') {
-      setIsInvalid(false); // Geçerli giriş
-    } else {
-      setIsInvalid(true); // Uygun olmayan giriş
-    }
-  };
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  // Formu submit etmeden önce validationları kontrol et
+  const errors = validateForm({ email, password });
+  if (Object.keys(errors).length > 0) {
+    setIsInvalid(true);
+    return;
+  }
+
+  // Formu server'a gönder
+};
+
+
+const validateForm = ({ email, password }) => {
+  const errors = {};
+
+  // Email adresinin geçerli olup olmadığını kontrol et
+  if (!email.includes('@')) {
+    errors.email = 'Lütfen geçerli bir e-posta adresi girin.';
+  }
+
+  // Şifrenin uzunluğunu kontrol et
+  if (password.length < 8) {
+    errors.password = 'Şifre en az 8 karakter uzunluğunda olmalıdır.';
+  }
+
+  // Şifrenin büyük ve küçük harf içerdiğini kontrol et
+  const letters = new Set(password.split(''));
+  if (!letters.has('A') || !letters.has('a')) {
+    errors.password = 'Şifre en az bir büyük harf ve en az bir küçük harf içermelidir.';
+  }
+
+  return errors;
+};
 
   return (
     <div>
       {randomNum !== null && (
-        <div className={`con ${isInvalid ? 'invalid-form' : ''}`}>
-          <div className="half">
-            <img
-              src={`https://picsum.photos/id/${randomNum}/750/715`}
-              alt={`Rastgele Resim ${randomNum}`}
-            />
-          </div>
-          <div className={`half loginbackgroundpart2 ${isInvalid ? 'invalid-form' : ''}`}>
-            <h1 className={`${isInvalid ? 'invalid-text' : ''}`}>
-              <Link to="/">
-                <span>
-                  <Button className="btnback-pos" size="md">
-                    <BsFillArrowLeftCircleFill size={25} />
-                  </Button>
-                </span>
-              </Link>
-              <h1 className="wavedesign1">
-                DFJGAODĞGFJSJDOANGPJODNG AĞOSGJDNAĞSOGLJFNAOGJLSDNOSLJDAGNOĞSGJDNAOSĞASFOALSFSDJANONGAOSP
-                ASDIOFGHĞASJDGOSDOGIJKASDGĞİSDGJ
-              </h1>
-            </h1>
-            <h1 className={`loginh1 ${isInvalid ? 'invalid-text' : ''}`}>
-              We <span className='kayan-yazi'>share</span> your future
-            </h1>
-            <Form className={`formic ${isInvalid ? 'invalid-form' : ''}`} onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button className="better-btn2" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </div>
-      )}
-
+  <div className={`con ${isInvalid ? 'invalid-form' : ''}`}>
+    <div className="half">
+      <img
+        src={`https://picsum.photos/id/${randomNum}/750/715`}
+        alt={`Rastgele Resim ${randomNum}`}
+      />
     </div>
-  )
-}
+    <div className={`half loginbackgroundpart2 ${isInvalid ? 'invalid-form' : ''}`}>
+      <h1 className={`${isInvalid ? 'invalid-text' : ''}`}>
+        <Link to="/">
+          <span>
+            <Button className="btnback-pos" size="md">
+              <BsFillArrowLeftCircleFill size={25} />
+            </Button>
+          </span>
+        </Link>
+        <h1 className="wavedesign1">
+          DFJGAODĞGFJSJDOANGPJODNG AĞOSGJDNAĞSOGLJ
+          FNAOGJLSDNOSLJDAGNOĞSGJDNAOSĞASFOALSFSDJ
+          ANONGAOSPASDIOFGHĞASJDGOSDOGIJKASDGĞİSDGJ
+        </h1>
+      </h1>
+      <h1 className={`loginh1 ${isInvalid ? 'invalid-text' : ''}`}>
+        We <span className='kayan-yazi'>share</span> your future
+      </h1>
+      <Form className={`formic ${isInvalid ? 'invalid-form' : ''}`} onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button className="better-btn2" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  </div>
+)}
+</div>
+  )}
 
 export default RandomImageGenerator;
