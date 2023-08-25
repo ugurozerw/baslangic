d0cc2a5da0a1d4f107e68b2464c62270291d474b
Ugur <ugurozerw@gmail.com>
2023-08-25 16:21:45 +0300

    aa

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -4,13 +4,14 @@ import Button from 'react-bootstrap/Button';
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
 import { Link } from "react-router-dom"
 import './cssforpages.css';
+import { FormText } from 'react-bootstrap';
 
 
 // Bozuk resimler
 const brokenImageNumbers = [138, 286, 968, 394, 561, 751, 332, 589, 207, 587, 
                             843, 224, 897, 636, 333, 462, 624, 748, 752, 761, 
                             934, 105, 720, 734, 753, 422, 750, 854, 470, 749,
-                            725, 359, 895, 647, 150];
+                            725, 359, 895, 647, 150, 414, 754];
 
 function RandomImageGenerator() {
   const [randomNum, setRandomNum] = useState(null);
@@ -98,31 +99,30 @@ const validateForm = ({ email, password }) => {
       <h1 className={`loginh1 ${isInvalid ? 'invalid-text' : ''}`}>
         We <span className='kayan-yazi'>share</span> your future
       </h1>
-      <Form className={`formic ${isInvalid ? 'invalid-form' : ''}`} onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+      <Form className={`form-step ${isInvalid ? 'invalid-form' : ''}`} onSubmit={handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>E-posta Adresi</Form.Label>
           <Form.Control
-            className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
             type="email"
-            placeholder="Enter email"
+            placeholder="E-posta adresinizi girin"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className="form-input"
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <FormText>We never give e-mail info to any 3rd party apps.</FormText>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
+        <Form.Group controlId="password">
+          <Form.Label>Şifre</Form.Label>
           <Form.Control
-            className={`shadowofcontrol ${isInvalid ? 'is-invalid' : ''}`}
             type="password"
-            placeholder="Password"
+            placeholder="Şifrenizi girin"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="form-input"
           />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
