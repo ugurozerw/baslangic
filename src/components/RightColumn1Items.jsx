200ad0ab27f00ffb2c9a252d1dd2334e52d0375f
Ugur <ugurozerw@gmail.com>
2023-08-28 11:42:49 +0300

    aa

--- src/components/RightColumn1Items.jsx
+++ src/components/RightColumn1Items.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
+import './RightColumn1Items.css'; // CSS dosyasını import edin
 
 const RightColumn1Items = () => {
   return (
-    <div className="button-container ">
-      <div>
-        <button className="sort-btns">Yeni Çikanlar</button>
-      </div>
-      <div>
-        <button className="sort-btns">İndirimler</button>
-      </div>
-      <div>
-        <button className="sort-btns">En ucuzdan sırala</button>
-      </div>
-      <div>
-        <button className="sort-btns">En pahalıdan sırala</button>
-      </div>
-      <div>
-        <button className="sort-btns">Lorem ipsum dolor sit amet.</button>
-      </div>
-      <div>
-        <button className="sort-btns">Lorem ipsum</button>
-      </div>
+    <div className="button-container">
+      <button className="sort-btns">Yeni Çikanlar</button>
+      <button className="sort-btns">İndirimler</button>
+      <button className="sort-btns">En ucuzdan sırala</button>
+      <button className="sort-btns">En pahalıdan sırala</button>
+      <button className="sort-btns">Lorem ipsum dolor sit amet.</button>
+      <button className="sort-btns">Lorem ipsum</button>
     </div>
   );
 }
