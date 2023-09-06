98de06ae7d986397941aa8079e69d9c9243bedd7
Ugur <ugurozerw@gmail.com>
2023-09-06 15:16:35 +0300

    t

--- src/pages/Hakkimizda.jsx
+++ src/pages/Hakkimizda.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import CustomNavbar from '../components/CustomNavbar'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Placeholder } from 'react-bootstrap';
 
 const Hakkimizda = () => {
   return (
     <>
     <CustomNavbar></CustomNavbar>
-    <div>Hakkimizda</div>
+
+    <Container>
+
+    <Row>
+        <Col sm={7}>
+          <Placeholder sm={12} bg="success"></Placeholder>
+          <img src="https://picsum.photos/id/17/745/160" alt="" />
+        </Col>
+        <Col sm={5}>
+          <Placeholder sm={12} bg="danger"></Placeholder>
+        </Col>
+    </Row>
+    </Container>
     </>
   )
 }
